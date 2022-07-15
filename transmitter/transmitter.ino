28dd1085946a22858ef411f156a515e3441228be
Lucas Dias Hudson <72749364+lucashudson2002@users.noreply.github.com>
2022-07-15 10:46:34 -0300

    Create transmitter.ino

---
+++ transmitter/transmitter.ino
@@ -0,0 +1,16 @@
+/*
+Program: NRF24L01 controll for the Controlled Car
+Author: Lucas Dias Hudson
+Date: July 15, 2022
+GitHub: https://github.com/lucashudson2002/Controlled-Car
+Prerequisites:
+  https://github.com/nRF24/RF24
+*/
+
+void setup() {
+
+}
+
+void loop() {
+
+}
